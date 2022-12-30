63596319a2cbe949fca5cc13edfdaad9c13698ad
David Canady <dmcanady@liberty.edu>
2022-12-30 11:38:43 -0500

    version 0.1.1

--- pyrestsdk/__init__.py
+++ pyrestsdk/__init__.py
@@ -3,5 +3,5 @@ from pyrestsdk.type.enum._feature_usage_flag import FeatureUsageFlag
 
 __all__ = ["AbstractServiceClient","FeatureUsageFlag"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __module_name__ = "pyrestsdk"
