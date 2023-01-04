7ad94a41b922125048affb6c8caa7b96fbdc4b11
michaeldcanady <michaeldcanady+github@gmail.com>
2023-01-03 19:35:14 -0500

    Accessing generic type param

--- pyrestsdk/request/_abstract_request.py
+++ pyrestsdk/request/_abstract_request.py
@@ -3,6 +3,10 @@ from typing import (
     TYPE_CHECKING,
     Dict,
     TypeVar,
+    Generic,
+    Type,
+    final,
+    get_args
 )
 
 # Internal Imports
@@ -10,22 +14,30 @@ from pyrestsdk import AbstractServiceClient
 
 B = TypeVar("B", bound="AbstractRequest")
 S = TypeVar("S", bound="AbstractServiceClient")
+T = TypeVar("T")
 
-class AbstractRequest(object):
-
-    _request_url: str
-    _client: S
+class AbstractRequest(Generic[T]):
 
     def __init__(self: B, request_url: str, client: S) -> None:
         
         super().__init__()
 
-        self._request_url = request_url
-        self._client = client
+        self._request_url: str = request_url
+        self._client: S = client
 
     @property
     def Client(self: B) -> S:
         return self._client
+    
+    @property
+    @final
+    def GenericType(self) -> Type[T]:
+        """Gets the the type argument provided
+        """
+        
+        _type: Type[T] = get_args(self.__orig_bases__[0])[0] # type: ignore  
+              
+        return _type
 
     @property
     def RequestUrl(self: B) -> str:
