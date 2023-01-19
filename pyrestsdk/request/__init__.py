ace069ba7134bbf0f16997baa953b5499b71c00c
David Canady <dmcanady@liberty.edu>
2023-01-19 14:29:51 -0500

    bad check

--- pyrestsdk/request/__init__.py
+++ pyrestsdk/request/__init__.py
@@ -2,7 +2,7 @@
 from sys import version_info
 from pyrestsdk.request._request import Request
 
-if version_info > (3,10):
+if version_info < (3,10):
     from pyrestsdk.request._base_request39 import BaseRequest
 else:
     from pyrestsdk.request._base_request310 import BaseRequest
