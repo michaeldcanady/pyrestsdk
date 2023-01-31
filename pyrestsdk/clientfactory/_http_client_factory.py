3e4bd80821b9c903b98ab628eefd04470d0d9a88
David Canady <dmcanady@liberty.edu>
2023-01-31 16:41:26 -0500

    adding basic implementation of HTTP Client Factory

--- pyrestsdk/clientfactory/_http_client_factory.py
+++ pyrestsdk/clientfactory/_http_client_factory.py
@@ -1,5 +1,5 @@
 """Houses Client Factory"""
-from typing import TypeVar, List
+from typing import TypeVar, List, Type
 from logging import getLogger
 
 from requests import Session
@@ -18,20 +18,40 @@ C = TypeVar("C", bound=AbstractBasicCredential)
 class HTTPClientFactory(AbstractHTTPClientFactory):
     """HTTP Client Factory"""
 
+    __slots__ = ["_base_url", "_protocol"]
+
+    def __init__(
+        self,
+        base_url: str,
+        session: Session,
+        protocol: str = "https",
+    ) -> None:
+        super().__init__(session)
+        self._base_url = base_url
+        self._protocol = protocol
+
     def create_with_custom_middleware(self, middleware: List[B]) -> Session:
 
         if not middleware:
             raise ValueError("Please provide a list of custom middleware")
-        self._register(middleware)
-        return self.session
 
-    def create_with_default_middleware(self, credential: C) -> Session:
-        return Session()
+        return self._register(middleware)
 
-    def _set_base_url(self, url: str) -> None:
+    def _set_base_url(self, sub_domain: str) -> None:
+
+        Logger.info("%s._set_base_url(): method called", type(self).__name__)
+
+        self.session.base_url = f"{self._protocol}://{sub_domain}.{self._base_url}" # type: ignore
+
+        Logger.debug(
+            "%s._set_base_url() : base url set to: %s",
+            type(self),
+            self.session.base_url, # type: ignore
+        )
+
         return
 
-    def _register(self, middleware: List[B]) -> None:
+    def _register(self, middleware: List[B]) -> Session:
         """
         Helper method that constructs a middleware_pipeline with the specified middleware
         """
@@ -43,4 +63,6 @@ class HTTPClientFactory(AbstractHTTPClientFactory):
             for ware in middleware:
                 middleware_pipeline.add_middleware(ware)
 
-            self.session.mount("https://", middleware_pipeline)
+            self.session.mount(f"{self._protocol}://", middleware_pipeline)
+
+        return self.session
---
+++ pyrestsdk/clientfactory/supports/__init__.py
@@ -0,0 +1,7 @@
+"""Supports extension Mixins"""
+
+from pyrestsdk.clientfactory.supports._supports_default_middleware import (
+    SupportsDefaultMiddleware,
+)
+
+__all__ = ["SupportsDefaultMiddleware"]
