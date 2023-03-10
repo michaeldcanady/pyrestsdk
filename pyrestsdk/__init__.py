b3448c01e1623ca26991c916590db8c869d30a38
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-03-10 08:26:48 -0500

    Update __init__.py

--- pyrestsdk/__init__.py
+++ pyrestsdk/__init__.py
@@ -5,5 +5,5 @@ from pyrestsdk.type.enum._feature_usage_flag import FeatureUsageFlag
 
 __all__ = ["AbstractServiceClient","FeatureUsageFlag"]
 
-__version__ = "0.3.8-beta4"
+__version__ = "0.4.0"
 __module_name__ = "pyrestsdk"
