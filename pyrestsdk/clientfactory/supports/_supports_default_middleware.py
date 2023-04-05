a4507ba01058583d404a7040419ad441800a79c5
David Canady <dmcanady@liberty.edu>
2023-04-05 11:47:21 -0400

    adding class doc string

--- pyrestsdk/clientfactory/supports/_supports_default_middleware.py
+++ pyrestsdk/clientfactory/supports/_supports_default_middleware.py
@@ -21,6 +21,23 @@ class SupportsDefaultMiddleware(ABC): #pylint: disable=too-few-public-methods
     Supports Default Middleware
     ===========================
     
+    This abstract base class is used to define an interface
+    for HTTP Clients that support registering and using default
+    middleware. Any class that extends this class is expected to
+    implement the _register and create_with_default_middleware
+    methods.
+
+    Usage::
+        # extend this class to create your HTTP Client
+        class MyHttpClient(SupportsDefaultMiddleware):
+            def __init__(self, *args, **kwargs) -> None:
+                super().__init__(*args, **kwargs)
+
+            def _register(self, middleware: List[B]) -> None:
+                # implementation details
+
+            def create_with_default_middleware(self) -> Session:
+                # implementation details
     """
 
     def __init__(self, *args, **kwargs) -> None:
